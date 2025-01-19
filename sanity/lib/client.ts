158b20a7b2918f3eab698d66c43a3bca2a2a2ebb
muhammedlayan12 <muhammedlayan12@gmail.com>
2025-01-20 02:22:06 +0500

    Hackathon-3

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,26 +1,19 @@
 import { createClient } from 'next-sanity';
 
+// Fetching environment variables directly from process.env
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION;
 
-
-import { apiVersion, dataset, projectId } from '../env';
-
-
-
-if (!projectId || !dataset) {
+if (!projectId || !dataset || !apiVersion) {
   throw new Error(
-    'Missing required environment variables: NEXT_PUBLIC_SANITY_PROJECT_ID or NEXT_PUBLIC_SANITY_DATASET'
+    'Missing required environment variables: NEXT_PUBLIC_SANITY_PROJECT_ID, NEXT_PUBLIC_SANITY_DATASET, or NEXT_PUBLIC_SANITY_API_VERSION'
   );
 }
 
-
-
 export const client = createClient({
-  projectId,    
-  
-  dataset,          
-  
-  apiVersion,       
-  
-  useCdn: true,     
-  
+  projectId,
+  dataset,
+  apiVersion,
+  useCdn: false, 
 });
